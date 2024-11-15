a658a971a596430386426aa83c35d9e4d40543d5
João Silva <jfms7s@gmail.com>
2024-11-15 16:32:41 +0000

    Time: 0 ms (100.00%) | Memory: 17.2 MB (95.21%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        
+        for (index,value) in enumerate(nums):
+
+            if value >= target:
+                return index
+            
+        return len(nums)
