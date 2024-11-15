fc35ffd9efb006c913f4d3776714c944a9d3b1bd
João Silva <jfms7s@gmail.com>
2024-11-15 16:27:12 +0000

    Time: 10 ms (18.86%) | Memory: 16.6 MB (42.14%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        roman_values = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000,
+        }
+        result = 0
+        previous = 0
+        for char in s[::-1]:
+            current = roman_values[char]
+            signal = 1 if current >= previous else -1
+
+            result = result + (current * signal)
+            previous = roman_values[char]
+        
+        return result
