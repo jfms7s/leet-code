45300dbc065cbe84edfb832d14c339dd96cf0eed
João Silva <jfms7s@gmail.com>
2024-11-16 01:37:26 +0000

    Time: 3 ms (68.23%) | Memory: 18.7 MB (68.55%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -3,8 +3,10 @@ from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
-        counter = Counter(nums)
-        for value,count in counter.items():
-            if count == 1:
-                return value
+        mask = 0
+
+        for i in nums:
+            mask = mask ^ i
+
+        return mask
         
