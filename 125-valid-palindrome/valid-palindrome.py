9b1107946139b22d2e56e35d8584e25c72d8e0d5
João Silva <jfms7s@gmail.com>
2024-11-16 01:04:25 +0000

    Time: 3 ms (98.84%) | Memory: 17.7 MB (30.56%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        s = "".join(filter(str.isalnum, s.lower()))
+        
+        s = "".join([c for c in s.lower() if c.isalnum()])
 
         return s == s[::-1]
