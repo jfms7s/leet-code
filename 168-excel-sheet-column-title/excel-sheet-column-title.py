6dcfae73b4ef2d58254ebad69e6ee4b8d011398b
João Silva <jfms7s@gmail.com>
2024-11-16 03:42:47 +0000

    Time: 0 ms (100.00%) | Memory: 16.7 MB (28.62%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -4,9 +4,6 @@ class Solution:
         result = ""
         start_char = ord("A")
 
-        if columnNumber == 0:
-            return "A"
-
         while columnNumber > 0:
             remainder = (columnNumber -1 ) % 26
             columnNumber = (columnNumber -1 ) // 26 
