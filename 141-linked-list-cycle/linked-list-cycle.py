a16e238adb6c4769ab245c588a44413848b63a56
João Silva <jfms7s@gmail.com>
2024-11-16 02:00:20 +0000

    Time: 40 ms (83.46%) | Memory: 18.9 MB (96.46%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -11,7 +11,7 @@ class Solution:
             
         fast = slow = ListNode (val = None, next = head)
         
-        while fast and fast.next is not None:
+        while fast.next and fast.next.next:
             fast = fast.next.next
             slow = slow.next
             if fast == slow:
