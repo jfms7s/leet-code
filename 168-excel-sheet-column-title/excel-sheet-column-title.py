72baacd42dbad7871ada1e2ac746ecb57399a314
João Silva <jfms7s@gmail.com>
2024-11-16 14:12:08 +0000

    Time: 0 ms (100.00%) | Memory: 16.5 MB (67.11%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -3,10 +3,10 @@ class Solution:
 
         result = ""
         start_char = ord("A")
-
+        base = 26
         while columnNumber > 0:
-            remainder = (columnNumber -1 ) % 26
-            columnNumber = (columnNumber -1 ) // 26 
+            remainder = (columnNumber -1 ) % base
+            columnNumber = (columnNumber -1 ) // base 
             result = chr(start_char+remainder) + result
 
         return result
