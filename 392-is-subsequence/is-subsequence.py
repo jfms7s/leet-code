d5f3dcf10957c2e50fe17590e63a7ba53f3e5a57
João Silva <jfms7s@gmail.com>
2025-05-18 15:21:04 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.53%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -9,11 +9,8 @@ class Solution:
 
         while s_index < len(s) and t_index < len(t):
 
-            current_character = s[s_index]
-
-            while t_index < len(t) and t[t_index] != current_character:
-                t_index += 1
+            if s[s_index] == t[t_index]:
+                s_index += 1
             t_index += 1
-            s_index += 1
         
-        return s_index == len(s) and t_index != (len(t) + 1)
+        return s_index == len(s)
