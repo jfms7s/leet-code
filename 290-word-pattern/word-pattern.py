b5c2f4ba2d4333d5ec98849ea9bb4e5e359e7856
João Silva <jfms7s@gmail.com>
2025-05-21 00:57:42 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (57.86%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -3,8 +3,11 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         
         split_s = s.split()
+        if len(pattern) != len(split_s):
+            return False
+
         pset = set(pattern)
         sset = set(split_s)
-        zip_set = set(zip_longest(pattern, split_s, fillvalue=""))
+        zip_set = set(zip(pattern, split_s))
         print((pset) , (sset) , (zip_set))
         return len(pset) == len(sset) == len(zip_set)
